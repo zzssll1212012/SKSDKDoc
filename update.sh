93407d7e6cf3a0578d311553079c92b427b00ec6
Void <zzssll1212012@163.com>
2024-03-08 14:19:28 +0800

    repair

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 拉取远程仓库的最新内容到本地的 main 分支
-git fetch origin main
+# 拉取远程仓库的最新内容到本地的 master 分支
+git fetch origin master
 
-# 切换到本地的 main 分支
-git checkout main
+# 切换到本地的 master 分支
+git checkout master
 
-# 合并远程仓库的 main 分支到本地的 main 分支
-git merge origin/main
+# 合并远程仓库的 master 分支到本地的 master 分支
+git merge origin/master
 
 # 进入 SKSDKDoc 文件夹
 cd /var/sdkdoc/SKSDKDoc/
